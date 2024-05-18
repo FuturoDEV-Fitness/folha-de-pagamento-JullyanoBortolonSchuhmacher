de6dcabc44002e23a36326d02a93b9b505b4b1fb
JullyanoBortolonSchuhmacher <jullyano_schuhmac@estudante.sesisenai.org.br>
2024-05-18 15:53:58 -0300

    Resolução do exercicio 03, com adição no "index.js" para que tenha interação entre os arquivos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 const calcularInss = require("./calculo_inss")
+const calcularImpostoDeRenda = require("./calculo_imposto_renda")
 
-console.log(calcularInss(4444))
+console.log(calcularInss(2123))
+console.log(calcularImpostoDeRenda(2123))
