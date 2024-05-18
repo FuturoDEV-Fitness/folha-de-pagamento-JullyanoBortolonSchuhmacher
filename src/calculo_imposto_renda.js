0059e202192b89bf913ad4a46a26f1031688c66b
JullyanoBortolonSchuhmacher <jullyano_schuhmac@estudante.sesisenai.org.br>
2024-05-18 20:54:06 -0300

    🐬Atualizando => trocando os let das  aliquitas para const, para que não haja problemas de marcar novos valores para as aliquotas, e retirando os valores a deduzir (não houve necessidade deles)

--- src/calculo_imposto_renda.js
+++ src/calculo_imposto_renda.js
@@ -2,17 +2,11 @@ function calcularImpostoDeRenda(salarioBruto) {
   let valorIR = 0
   
 //   valor das aliquotas
-  let aliquota1 = 7.5/100
-  let aliquota2 = 15/100
-  let aliquota3 = 22.5/100
-  let aliquota4 = 27.5/100
+  const aliquota1 = 7.5/100
+  const aliquota2 = 15/100
+  const aliquota3 = 22.5/100
+  const aliquota4 = 27.5/100
 
-//   Parcelas a reduzir
-  const parcelaCalculo1 = 158.40
-  const parcelaCalculo2 = 370.40
-  const parcelaCalculo3 = 651.73
-  const parcelaCalculo4 = 884.96
-  
 //   Salários bases de contribuição
   const salarioBaseCalculo1 = 2112
   const salarioBaseCalculo2 = 2826.65
@@ -21,24 +15,12 @@ function calcularImpostoDeRenda(salarioBruto) {
   
   if (salarioBruto > salarioBaseCalculo1 && salarioBruto <= salarioBaseCalculo2) {
     valorIR = salarioBruto * aliquota1
-    if (valorIR > parcelaCalculo1) {
-        valorIR = parcelaCalculo1
-    }
   } else if (salarioBruto > salarioBaseCalculo2 && salarioBruto <= salarioBaseCalculo3) {
     valorIR = salarioBruto * aliquota2
-    if (valorIR > parcelaCalculo2) {
-        valorIR = parcelaCalculo2
-    }
   } else if (salarioBruto > salarioBaseCalculo3 && salarioBruto <= salarioBaseCalculo4) {
     valorIR = salarioBruto * aliquota3
-    if (valorIR > parcelaCalculo3){
-        valorIR = parcelaCalculo3
-    }
   } else if (salarioBruto > salarioBaseCalculo4) {
     valorIR = salarioBruto * aliquota4
-    if (valorIR > parcelaCalculo4){
-        valorIR = parcelaCalculo4
-    }
   } else {
     valorIR = salarioBruto * 0 
   }
