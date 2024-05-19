d4ec3fc174ce93a70432e53d6779ba25a91b393f
JullyanoBortolonSchuhmacher <jullyano_schuhmac@estudante.sesisenai.org.br>
2024-05-19 17:11:45 -0300

    instalação do pdfkit + realização do exercicio 05 🍃

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ const calcularInss = require("./calculo_inss")
 const calcularImpostoDeRenda = require("./calculo_imposto_renda")
 const calculoSalarioLiquido = require("./calculo_salario_liquido")
 
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+const doc = new PDFDocument();
+
 let nome = ''
 let data = new Date()
 let cpf = ''
+let gerarPdf = ''
 
 const readline = require('readline')
 
@@ -19,7 +24,6 @@ input.question("Qual seu nome? ", (nomeInserido) => {
         input.question("Qual seu CPF? ", (cpfInserido) => {
             cpf = cpfInserido.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
             input.question("Qual o valor do salário bruto? ", (salarioBruto) => {
-                input.close()
                 console.log("--- folha de pagamento ---")
                 console.log(`Nome: ${nome}`)
                 console.log(`CPF: ${cpf}`)
@@ -30,6 +34,34 @@ input.question("Qual seu nome? ", (nomeInserido) => {
                 console.log(`Salário Líquido: R$ ${calculoSalarioLiquido(salarioBruto)}`)
                 console.log("--- --- --- --- --- --- ---")
                 console.log(`Solicitação em ${data.toLocaleDateString()} as ${data.toLocaleTimeString()}`)
+                input.question("Deseja gerar em PDF?(S/N)", (desejaGerarPdf) => {
+                    gerarPdf = desejaGerarPdf.trim().toLowerCase()
+                    if (gerarPdf == "s" || gerarPdf == "sim") {
+                        console.log("Gerando PDF...")
+                        doc.pipe(fs.createWriteStream(`folha_de_pagamento_${nome}.pdf`));
+                        doc.fontSize(25);
+                        doc.text("Folha de Pagamento")
+                        doc.fontSize(12);
+                        // linha
+                        doc.text("--- ---");
+                        doc.text(`Data de Geração: ${data.toLocaleDateString()}`);
+                        doc.text(`Nome: ${nome}`);
+                        doc.text(`CPF: ${cpf}`);
+                        // linha
+                        doc.text("--- ---");
+                        doc.text(`Salário Bruto: R$ ${salarioBruto}`)                            
+                        doc.text("--- ---");
+                        doc.text(`INSS: R$ ${calcularInss(salarioBruto)}`)                            
+                        doc.text(`Imposto de Renda: R$ ${calcularImpostoDeRenda(salarioBruto)}`)                            
+                        doc.text("--- ---");
+                        doc.text(`Salário Líquido: R$ ${calculoSalarioLiquido(salarioBruto)}`)                            
+                        doc.end()
+                        console.log("PDF gerado!")
+                    } else {
+                        console.log("Programa encerrado!")
+                    }
+                    input.close()
+                })
             })
         })
     })
